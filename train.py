3e5b45c2494076c0dbe0d5848a4773bfe45411a4
WinterBelieve <winterbelieveyuan@gmail.com>
2024-03-14 21:59:06 +0800

    Your commit message

--- train.py
+++ train.py
@@ -40,10 +40,18 @@ train_ds, test_ds = torch.utils.data.random_split(full_ds, [train_size, test_siz
 train_dl = torch.utils.data.DataLoader(train_ds, batch_size=32, shuffle=True)
 test_dl = torch.utils.data.DataLoader(test_ds, batch_size=32, shuffle=False)
 
-# Define the model
+# define model
 model = torch.nn.Sequential(
+    # input: [32, 28, 28]
+    torch.nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),  # output: [32, 28, 28]
+    torch.nn.ReLU(),
+    torch.nn.MaxPool2d(kernel_size=2, stride=2),  # output: [32, 14, 14]
+
+    torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # output: [64, 14, 14]
+    torch.nn.MaxPool2d(kernel_size=2, stride=2),  # output: [64, 7, 7]
+
     torch.nn.Flatten(),
-    torch.nn.Linear(2352, 128),
+    torch.nn.Linear(64 * 7 * 7, 128),
     torch.nn.ReLU(),
     torch.nn.Linear(128, 10),
     torch.nn.LogSoftmax(dim=1)
