e3e2a0e89fca08f04dd0384349debda67a1b1530
WinterBelieve <winterbelieveyuan@gmail.com>
2024-03-18 18:01:12 +0800

    Your commit message

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ model = nn.Sequential(
 
 # 定義損失函數和優化器
 loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 # 訓練模型
 EPOCHS = 10
