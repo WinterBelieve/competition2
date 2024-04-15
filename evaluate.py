89215536df011fcb5851f16a269bfd0ae60a6a74
WinterBelieve <winterbelieveyuan@gmail.com>
2024-04-16 01:58:30 +0800

    test

--- evaluate.py
+++ evaluate.py
@@ -29,18 +29,16 @@ if len(sys.argv) > 1:
 model = torch.jit.load(model_path).to(device)
 
 # model evaluation
-def evaluate_model(model, loader):
-    model.eval()
+def evaluate_model(model, test_loader):
+    model.eval()  # set model to evaluation mode
     correct = 0
     total = 0
-    with torch.no_grad():
-        for images, labels in loader:
-            images, labels = images.to(device), labels.to(device)
+    with torch.no_grad():  # no gradient
+        for images, labels in test_loader:
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
-            correct += (predicted == labels).sum().item()
             total += labels.size(0)
-    
+            correct += (predicted == labels).sum().item()
     accuracy = 100 * correct / total
     print(json.dumps({"total": total, "correct": correct, "accuracy": accuracy}))
 
