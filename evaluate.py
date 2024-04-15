68c8a17c741cdf3641f806308583123736606363
WinterBelieve <winterbelieveyuan@gmail.com>
2024-04-16 01:19:47 +0800

    test

--- evaluate.py
+++ evaluate.py
@@ -1,39 +1,45 @@
 import torch
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
-from torch import nn
 import json
 import sys
-import os
 
-# 圖像轉換
+# check if model path is provided
+if len(sys.argv) < 2:
+    print("Usage: python evaluate.py <model_path>")
+    sys.exit(1)
+
+# device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# transform
 transform = transforms.Compose([
-    transforms.Resize((64, 64)),  # 將圖像大小調整為 64x64
+    transforms.Resize((64, 64)),  # resize
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # 標準化
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # normalize
 ])
 
-# 載入測試數據集
+# data
 data_dir = 'Traditional-Chinese-Handwriting-Dataset/data/cleaned_data(50_50)'
 test_data = datasets.ImageFolder(root=data_dir, transform=transform)
-num_classes = len(test_data.classes)  # 假設模型和數據集已經準備好
-# 假設測試數據集占總數據集的後20%
 
+# if you want to split the data into train and test
 _, test_data = torch.utils.data.random_split(test_data, [int(len(test_data)*0.8), len(test_data)-int(len(test_data)*0.8)])
 
 test_loader = DataLoader(test_data, batch_size=128, shuffle=True)
-# 加載模型參數
-# model_path = '/home/jovyan/competition2/handwrite_model.pth'
-model_path = sys.argv[1]  # 模型路徑作為命令行第一個參數傳入
-model = torch.jit.load(model_path)
 
-# 模型評估
+# model path
+model_path = sys.argv[1]  
+model = torch.jit.load(model_path).to(device)
+
+# model evaluation
 def evaluate_model(model, test_loader):
-    model.eval()  # 設置為評估模式
+    model.eval()  # set model to evaluation mode
     correct = 0
     total = 0
-    with torch.no_grad():  # 在這段代碼中不計算梯度
+    with torch.no_grad():  # no need to calculate gradients
         for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
@@ -41,5 +47,5 @@ def evaluate_model(model, test_loader):
     accuracy = 100 * correct / total
     print(json.dumps({"total": total, "correct": correct, "accuracy": accuracy}))
 
-# 執行模型評估
+# evaluate model
 evaluate_model(model, test_loader)
