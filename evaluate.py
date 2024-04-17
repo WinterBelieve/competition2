2835221ece300f347790d9d7f3f1aa8faa8a5420
WinterBelieve <winterbelieveyuan@gmail.com>
2024-04-17 20:17:45 +0800

    test

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,16 @@
 import torch
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
-import json
+from torch import nn
 import sys
+import json
+
+# set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # transform
 transform = transforms.Compose([
-    transforms.Resize((64, 64)),  # resize to 64x64 pixels
+    transforms.Resize((64, 64)),  # resize
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # normalize
 ])
@@ -17,25 +21,25 @@ dataset = datasets.ImageFolder(root=data_dir, transform=transform)
 _, test_dataset = torch.utils.data.random_split(dataset, [int(len(dataset) * 0.8), len(dataset) - int(len(dataset) * 0.8)])
 test_loader = DataLoader(test_dataset, batch_size=128)
 
-# Assuming the model path is provided as a command-line argument
 # model_path = 'handwrite_model.pth'
 model_path = sys.argv[1]
-model = torch.jit.load(model_path).to('cpu')  # ensure the model is on CPU
+model = torch.jit.load(model_path).to(device)
 
-# model evaluation
+# model
 def evaluate_model(model, loader):
-    model.eval()  # set the model to evaluation mode
+    model.eval()
     correct = 0
     total = 0
-    with torch.no_grad():  # do not compute gradients
+    with torch.no_grad():
         for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
             correct += (predicted == labels).sum().item()
             total += labels.size(0)
-    
+            
     accuracy = 100 * correct / total  # calculate accuracy percentage
     print(json.dumps({"total": total, "correct": correct, "accuracy": accuracy}))  # print results in JSON format
 
-# evaluate model
+# evaluate
 evaluate_model(model, test_loader)
